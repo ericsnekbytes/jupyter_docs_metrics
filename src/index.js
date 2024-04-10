3a950c51a387aeb540beb15702cb40ba5a9fba0b
Eric Gentry <ericsnekbytes@gmail.com>
2024-04-10 12:07:06 -0400

    Add date info.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 
 function processDates() {
     for (item of document.getElementsByClassName('latest_date_info')) {
+        
         let raw = item.innerText.split(',');
+        console.log("WIK WIK")
+        console.log(item.innerText)
         let elapsed = new Date() - new Date(raw[0], raw[1] - 1, raw[2]);
         item.innerText = 'Days since last datapoint: ~' + (elapsed / (1000 * 60 * 60 * 24));
+        item.style.visibility = "visible";
     }
 }
 
